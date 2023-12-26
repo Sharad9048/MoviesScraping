d09ae5cd991a4690f48ff0908fdf44c39eb75dc2
Sharad9048 <sharaddasari9048@gmail.com>
2023-12-26 12:33:32 +0530

    Logger from error to info

--- MovieScraping.py
+++ MovieScraping.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-from functools import reduce
-import pandas as pd
+
 import logging, os
 
+# Configuring Loggging program
 logging.basicConfig(
-    level = logging.ERROR,
-    format = "%(asctime)s %(levelname)s %(message)s",
+    level = logging.INFO,       # It will store logs whose severity greater than INFO
+    format = "%(asctime)s %(levelname)s %(message)s",       # It is the format in which the logging will b
     datefmt = "%Y-%m-%d %H:%M:%S",
     filename= "scraping.log"
 )
 
+# Process ID will be used for storing logs
 processId = os.getpid()
 
 # This URL will used for navigating to between pages and individual movie details
@@ -18,9 +19,6 @@ baseUrl = 'https://www.themoviedb.org'
 
 # This will help us from being caught by the BOT on the above website
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
-
-# Making the List of 50 page URL
-urls = [baseUrl+f'/movie?page={pageNo}' for pageNo in range(1,51)]
 
 def tryRequestingData(url):
     """
@@ -43,18 +41,16 @@ def tryRequestingData(url):
             # Checking if number of retries has finished
             if retryCount <=0:
                 logging.error("Failed to get Response 200 from URL: {url}")
-                logging.info("Safely terminating...")
-                exit()          # The program will be terminated
+                raise RuntimeError("No Response 200")
             else:
                 retry-=1        # Decrementing number of Retries
         
-        # Re-attempting to get the HTML data
-        sourceData = requests.get(url, headers=header)
+            # Re-attempting to get the HTML data
+            sourceData = requests.get(url, headers=header)
 
     except:
         logging.error(f"Invalid URL: {url}")
-        logging.info("Safely terminating...")
-        exit()
+        raise RuntimeError("Invalid URL")
 
     return sourceData.text
 
@@ -73,8 +69,8 @@ def getMovieDetail(movieSoupData):
                 "Rating" : "72",
                 "Genres" : "Comedy,Family,Fantasy",
                 "Runtime" : "1h 57m",
-                "Overview" : "Movie Description",
-                "Director" : "Movie Director",
+                "Overview" : "The MC is very strong",
+                "Director" : "Jake",
             }
 
     The will extract the following data from the movie block and movie detail page
@@ -95,9 +91,21 @@ def getMovieDetail(movieSoupData):
 
     # Extracting the path for navigating to the movie detail page
     moviePath = movieSoupData.find('h2').find('a').get('href')
-
-    # Extracting the movie detail HTML page using the path stored above
-    movieSourceData = tryRequestingData(baseUrl + moviePath)
+    try:
+        # Extracting the movie detail HTML page using the path stored above
+        movieSourceData = tryRequestingData(baseUrl + moviePath)
+    except RuntimeError:
+        logging.info(f'{processId}|rating,genres,runtime,overview,director|movie detail page not found|NA|{movieTitle}|{baseUrl + moviePath}')
+        movieData = {
+            "Title" : movieTitle,
+            "Date" : movieDate,
+            "Rating" : "NA",
+            "Genres" : "NA",
+            "Runtime" : "NA",
+            "Overview" : "NA",
+            "Director" : "NA",
+        }
+        return movieData
 
     # Converting HTML string to BeautifulSoup object
     movieDetailPage = BeautifulSoup(movieSourceData, 'lxml')
@@ -117,7 +125,7 @@ def getMovieDetail(movieSoupData):
             if rating == "NR":
                 rating = 'NA'
         except AttributeError:
-            logging.error(f'{processId}|rating|data not found|NA|{movieTitle}|{baseUrl + moviePath}')
+            logging.info(f'{processId}|rating|element not found|NA|{movieTitle}|{baseUrl + moviePath}')
             rating = 'NA'
     except:
         logging.error(f'{processId}|rating|operation failed|NA|{movieTitle}|{baseUrl + moviePath}')
@@ -131,7 +139,7 @@ def getMovieDetail(movieSoupData):
             # Joining all the genre string with comma seperated string
             genres =','.join(genresList)
         except AttributeError:
-            logging.error(f'{processId}|genres|data not found|NA|{movieTitle}|{baseUrl + moviePath}')
+            logging.info(f'{processId}|genres|element not found|NA|{movieTitle}|{baseUrl + moviePath}')
             genres = 'NA'
     except:
         logging.error(f'{processId}|genres|operation failed|NA|{movieTitle}|{baseUrl + moviePath}')
@@ -143,7 +151,7 @@ def getMovieDetail(movieSoupData):
             # Extraction Runtime data of the movie inside span elemnet of class 'runtime'
             runtime = movieDetailPage.find('span', class_ = 'runtime').text.replace('\n','').strip()
         except AttributeError:
-            logging.error(f'{processId}|runtime|data not found|NA|{movieTitle}|{baseUrl + moviePath}')
+            logging.info(f'{processId}|runtime|element not found|NA|{movieTitle}|{baseUrl + moviePath}')
             runtime = 'NA'
     except:
         logging.error(f'{processId}|runtime|operation failed|NA|{movieTitle}|{baseUrl + moviePath}')
@@ -154,7 +162,7 @@ def getMovieDetail(movieSoupData):
             # Extraction overview of the movie inside p tag
             overview = movieDetailPage.find('p').text
         except AttributeError:
-            logging.error(f'{processId}|overview|data not found|NA|{movieTitle}|{baseUrl + moviePath}')
+            logging.info(f'{processId}|overview|element not found|NA|{movieTitle}|{baseUrl + moviePath}')
             overview = 'NA'
     except:
         logging.error(f'{processId}|overview|operation failed|NA|{movieTitle}|{baseUrl + moviePath}')
@@ -175,7 +183,7 @@ def getMovieDetail(movieSoupData):
                     # Ending the loop since the director detail was found
                     break
         except AttributeError:
-            logging.error(f'{processId}|director|data not found|NA|{movieTitle}|{baseUrl + moviePath}')
+            logging.info(f'{processId}|director|data not found|NA|{movieTitle}|{baseUrl + moviePath}')
             director = 'NA'
     except:
         logging.error(f'{processId}|director|operation failed|NA|{movieTitle}|{baseUrl + moviePath}')
@@ -198,9 +206,14 @@ def getAllMoviesDetail(url):
     """
     @input:str - Accepts only URL
     @output:list - Return a List of movie data Dictionary inside a List
+
+    It will Extract all the movie data from URL HTML page
     """
-    # Getting HTML data from the url
-    sourceData = tryRequestingData(url)
+    try:
+        # Getting HTML data from the url
+        sourceData = tryRequestingData(url)
+    except RuntimeError:
+        return []
     
     # Converting the HTML string data to soup data
     popularMoviesPage = BeautifulSoup(sourceData, 'lxml')
@@ -213,17 +226,26 @@ def getAllMoviesDetail(url):
 
     return movieDetailList
 
-# Extracting movie detail using the list of urls of 50 pages
-pagesMovieDetailList = map(getAllMoviesDetail, urls)
 
-# Reducing collection of list of movie details to list of movie detail
-movieDetailList = reduce(lambda list1, list2: list1 + list2, pagesMovieDetailList)
+if __name__=="__main__":
 
-# Converting the compressed data into a tuple
-allMoviesData = tuple(movieDetailList)
+    import pandas as pd
+    from functools import reduce
 
-# Creating a pandas dataframe from the tuple of movie detail
-moviesDf = pd.DataFrame(allMoviesData)
+    # Making the List of 50 page URL
+    urls = [baseUrl+f'/movie?page={pageNo}' for pageNo in range(1,51)]
 
-# Saving the dataframe in an excel file
-moviesDf.to_excel('Movies.xlsx')
+    # Extracting movie detail using the list of urls of 50 pages
+    pagesMovieDetailList = map(getAllMoviesDetail, urls)
+
+    # Reducing collection of list of movie details to list of movie detail
+    movieDetailList = reduce(lambda list1, list2: list1 + list2, pagesMovieDetailList)
+
+    # Converting the compressed data into a tuple
+    allMoviesData = tuple(movieDetailList)
+
+    # Creating a pandas dataframe from the tuple of movie detail
+    moviesDf = pd.DataFrame(allMoviesData)
+
+    # Saving the dataframe in an excel file
+    moviesDf.to_excel('Movies.xlsx')
